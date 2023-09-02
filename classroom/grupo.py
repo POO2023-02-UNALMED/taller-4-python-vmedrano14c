db6f3d995bdf6acde5bba3a7c5569e64d31c11f5
vmedrano14c <141966048+vmedrano14c@users.noreply.github.com>
2023-09-02 18:16:10 -0500

    Update grupo.py

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -12,26 +12,19 @@ class Grupo:
         for asignatura_nombre in kwargs.values():
             self._asignaturas.append(Asignatura(asignatura_nombre))
 
-
     def agregarAlumno(self, alumno, lista=None):
         if lista is None:
             lista = []
-            lista.append(alumno)
-            self.listadoAlumnos = self.listadoAlumnos + lista
+        lista.append(alumno)
+        self.listadoAlumnos += lista
 
     def __str__(self):
-        return f"Grupo:{self._grupo}, Asignaturas: {','.join(str(asig) for asig in self._asignaturas)}, Alumnos :{','.join(self.listadoAlumnos)}"
+        asignaturas_str = ",".join(str(asig) for asig in self._asignaturas)
+        alumnos_str = ",".join(self.listadoAlumnos)
+        return f"Grupo:{self._grupo}, Asignaturas: {asignaturas_str}, Alumnos: {alumnos_str}"
 
-    @ classmethod
+    @classmethod
     def asignarNombre(cls, nombre="Grado 10"):
-        cls.grado = nombre
-
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 6"):
-        cls.grado = nombre
-
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
         cls.grado = nombre
     
 
